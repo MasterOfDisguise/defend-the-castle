9687ce578e64f11f2fea1d8a0791015659a0200c
MasterOfDisguise <simonsnider@gmail.com>
2015-04-07 11:38:58 -0700

    added days

--- classes.py
+++ classes.py
@@ -20,25 +20,40 @@ class Castle(pygame.sprite.Sprite):
 
 class Enemy(pygame.sprite.Sprite):
 
-    def __init__(self, manager, speed, health, damage):
+    def __init__(self, manager, type, spawn_time):
         pygame.sprite.Sprite.__init__(self)
         self.manager = manager
-        self.speed = speed
-        self.health = health
-        self.damage = damage
+        self.type = type
+        if self.type == 'wimp':
+            self.damage = 2
+            self.speed = 2
+            self.health = 2
+        if self.type == 'horseman':
+            self.damage = 1
+            self.speed = 4
+            self.health = 2
+        if self.type == 'bruiser':
+            self.damage = 2
+            self.speed = 1
+            self.health = 10
+        self.spawn_time = spawn_time
+        self.spawned = False
         self.surface = self.manager.surface
         self.image = pygame.image.load("enemy.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = 0
-        self.rect.y = 500
+        self.rect.y = random.randint(480, 520)
         self.timer = 0
         self.timer_speed = 1
+        self.health_bar_width = 31
+        self.pixels_per_life = self.health_bar_width/float(self.health)
 
     def move(self):
-        self.rect.x += self.speed
-        if pygame.sprite.collide_rect(self, self.manager.castle):
-            self.speed = 0
-            self.attack()
+        if self.spawned:
+            self.rect.x += self.speed
+            if pygame.sprite.collide_rect(self, self.manager.castle):
+                self.speed = 0
+                self.attack()
 
     def attack(self):
         self.timer += self.timer_speed
@@ -55,16 +70,69 @@ class Enemy(pygame.sprite.Sprite):
         if self.timer == 80:
             self.timer_speed *= -1
             if pygame.sprite.collide_rect(self, self.manager.castle):
-                self.manager.life -= self.damage
+                saved_data["life"] -= self.damage
                 self.manager.health_bar_width -= self.manager.pixels_per_life*self.damage
         if self.timer == 0:
             self.timer_speed *= -1
 
-    def update(self):
-        self.move()
+    def take_damage(self, damage):
+        self.health -= damage
         if self.health <= 0:
             self.kill()
             self.manager.manager.money += 100
+        self.health_bar_width -= damage*self.pixels_per_life
+
+    def draw(self):
+        self.surface.blit(self.image, self.rect)
+        pygame.draw.rect(self.surface, red, (self.rect.x, self.rect.y-20, 31, 10), 0)
+        pygame.draw.rect(self.surface, green, (self.rect.x, self.rect.y-20, self.health_bar_width, 10), 0)
+
+    def update(self):
+        self.move()
+        if self.spawn_time >= 0:
+            self.spawn_time -= 1
+
+
+class Day():
+
+    def __init__(self, manager):
+        self.manager = manager
+        self.day = saved_data['day']
+        self.timer = 0
+        self.enemies = pygame.sprite.Group()
+        self.spawn_waves()
+
+    def spawn_waves(self):
+        if self.day == 1:
+            for i in range(10):
+                enemy = Enemy(self.manager, 'wimp', random.randint(0, 2000))
+                self.enemies.add(enemy)
+        if self.day == 2:
+            for i in range(20):
+                enemy = Enemy(self.manager, 'wimp', random.randint(0, 2000))
+                self.enemies.add(enemy)
+        if self.day == 3:
+            for i in range(20):
+                enemy = Enemy(self.manager, 'wimp', random.randint(0, 2000))
+                self.enemies.add(enemy)
+            for i in range(3):
+                enemy = Enemy(self.manager, 'horseman', random.randint(0, 2000))
+                self.enemies.add(enemy)
+
+    def end_day(self):
+        saved_data['day'] += 1
+        self.manager.manager.screen = MainMenu(self.manager.manager)
+
+    def update(self):
+        self.timer += 1
+        self.enemies.update()
+        if len(self.manager.enemies) == 0 and self.timer >= 2000:
+            self.end_day()
+        for enemy in self.enemies:
+            if enemy.spawn_time <= 0:
+                enemy.spawned = True
+                self.manager.enemies.add(enemy)
+                self.enemies.remove(enemy)
 
 
 class Turret(pygame.sprite.Sprite):
@@ -91,8 +159,11 @@ class Turret(pygame.sprite.Sprite):
             target_list.append(enemy)
         if self.count >= self.fire_counter:
             self.image = pygame.image.load("turret fire.png").convert_alpha()
-            self.target = random.choice(target_list)
-            self.target.health -= 1*saved_data["turret_level"]
+            self.target = Enemy(self.manager, 'wimp', 0)
+            for enemy in target_list:
+                if enemy.rect.x >= self.target.rect.x:
+                    self.target = enemy
+            self.target.take_damage(saved_data["turret_level"])
             self.count = 0
 
     def update(self):
@@ -113,11 +184,27 @@ class Upgrade(pygame.sprite.Sprite):
 
     def activate_effect(self):
         if self.effect == 'damage_up':
-            saved_data["damage"] += 1
-            saved_data["upgrade_price"] *= 2
-        if self.effect == 'life_up':
-            saved_data["life"] *= 2
-            saved_data["upgrade_price"] *= 2
+            if self.manager.money >= saved_data["damage_upgrade_price"]:
+                saved_data["damage"] += 1
+                self.manager.money -= saved_data["damage_upgrade_price"]
+                saved_data["damage_upgrade_price"] *= 2
+        elif self.effect == 'life_up':
+            if self.manager.money >= saved_data["life_upgrade_price"]:
+                saved_data["life_max"] *= 2
+                saved_data['life'] = saved_data['life_max']
+                self.manager.money -= saved_data["life_upgrade_price"]
+                saved_data["life_upgrade_price"] *= 2
+        elif self.effect == 'turret_up':
+            if self.manager.money >= saved_data["turret_upgrade_price"]:
+                saved_data["turret_level"] += 1
+                self.manager.money -= saved_data["turret_upgrade_price"]
+                saved_data["turret_upgrade_price"] *= 3
+        elif self.effect == 'repair':
+            if self.manager.money >= 100:
+                saved_data["life"] += 100
+                if saved_data["life"] > saved_data["life_max"]:
+                    saved_data['life'] = saved_data["life_max"]
+                self.manager.money -= 100
 
 
 class GameScreen():
@@ -126,15 +213,13 @@ class GameScreen():
         self.manager = manager
         self.surface = self.manager.surface
         self.castle = Castle(self.manager)
-        self.life = saved_data["life_max"]
-        self.health_bar_width = 200
-        self.pixels_per_life = self.health_bar_width/float(self.life)
+        self.pixels_per_life = 200/float(saved_data["life_max"])
+        self.life_lost = self.pixels_per_life * (saved_data["life_max"] - saved_data["life"])
+        self.health_bar_width = 200 - self.life_lost
         self.enemies = pygame.sprite.Group()
         self.turret = Turret(self)
-        self.money_counter = eztext.Input(maxlength=45, color=white, prompt='money: '+str(self.manager.money))
-        self.button = eztext.Input(maxlength=45, color=white, prompt='main menu')
-        self.button.set_pos(width-120, 0)
         self.damage = saved_data['damage']
+        self.day = Day(self)
         self.timer = 0
 
     def handle_events(self, event):
@@ -142,29 +227,25 @@ class GameScreen():
             x, y = pygame.mouse.get_pos()
             for enemy in self.enemies:
                 if enemy.rect.collidepoint(x, y):
-                    enemy.health -= self.damage
-            if x >= width-120 and y <= 20:
-                self.manager.screen = MainMenu(self.manager)
+                    enemy.take_damage(self.damage)
         if event.type == pygame.KEYDOWN:
             self.manager.end_game()
 
     def draw(self):
         self.castle.draw()
         self.turret.draw()
-        self.enemies.draw(self.surface)
-        self.button.draw(self.surface)
+        for enemy in self.enemies:
+            enemy.draw()
         health_bar = pygame.draw.rect(self.surface, red, (self.castle.rect.x, self.castle.rect.y-40, 200, 20), 0)
         health_bar2 = pygame.draw.rect(self.surface, green, (self.castle.rect.x, self.castle.rect.y-40, self.health_bar_width, 20), 0)
 
     def update(self):
         self.timer += 1
-        if self.timer >= 100:
-            enemy = Enemy(self, 1, 2, 1)
-            self.enemies.add(enemy)
-            self.timer = 0
+        self.day.update()
         self.enemies.update()
         self.turret.update()
-        if self.life <= 0:
+        if saved_data["life"] <= 0:
+            saved_data["life"] = saved_data["life_max"]
             self.manager.end_game()
 
 
@@ -173,7 +254,7 @@ class MainMenu():
     def __init__(self, manager):
         self.manager = manager
         self.surface = self.manager.surface
-        self.button1 = eztext.Input(maxlength=45, color=white, prompt='play')
+        self.button1 = eztext.Input(maxlength=45, color=white, prompt='begin day')
         self.button1.set_pos(width/2, height*1/4)
         self.button2 = eztext.Input(maxlength=45, color=white, prompt='shop')
         self.button2.set_pos(width/2, height*3/4)
@@ -185,10 +266,12 @@ class MainMenu():
     def handle_events(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            if 400 <= x <= 440 and 160 <= y <= 170:
+            if 400 <= x <= 500 and 160 <= y <= 170:
                 self.manager.screen = GameScreen(self.manager)
             if 400 <= x <= 450 and 455 <= y <= 475:
                 self.manager.screen = Shop(self.manager)
+        if event.type == pygame.KEYDOWN:
+            self.manager.reset()
 
     def update(self):
         pass
@@ -211,8 +294,12 @@ class Shop():
     def init_upgrades(self):
         upgrade = Upgrade(self.manager, 50, 155, 'dmg upgrade.png', 'damage_up')
         upgrade1 = Upgrade(self.manager, 150, 155, 'life upgrade.png', 'life_up')
+        upgrade2 = Upgrade(self.manager, 250, 155, 'turret.png', 'turret_up')
+        repair = Upgrade(self.manager, 50, height-64, 'repair.png', 'repair')
         self.upgrades.add(upgrade)
         self.upgrades.add(upgrade1)
+        self.upgrades.add(upgrade2)
+        self.upgrades.add(repair)
 
     def handle_events(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -221,9 +308,7 @@ class Shop():
                 self.manager.screen = MainMenu(self.manager)
             for upgrade in self.upgrades:
                 if upgrade.rect.collidepoint(x, y):
-                    if self.manager.money >= saved_data['upgrade_price']:
-                        self.manager.money -= saved_data['upgrade_price']
-                        upgrade.activate_effect()
+                    upgrade.activate_effect()
 
     def update(self):
         pass
@@ -243,6 +328,17 @@ class Game():
 
     def handle_events(self, event):
         self.screen.handle_events(event)
+
+    def reset(self):
+        self.money = 0
+        saved_data["life_max"] = 100
+        saved_data["damage"] = 1
+        saved_data["turret_level"] = 0
+        saved_data["damage_upgrade_price"] = 100
+        saved_data["turret_upgrade_price"] = 500
+        saved_data["life_upgrade_price"] = 100
+        saved_data["life"] = 100
+        self.end_game()
 
     def end_game(self):
         saved_data['money'] = self.money
